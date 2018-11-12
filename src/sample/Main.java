c0fff4d13247ee5cfb900f962ab6a04e801053c5
Pauuukin <oleg.paukin@mail.ru>
2018-11-12 18:56:19 +0300

    test1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,8 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        System.out.println("hello");System.out.println("hello");System.out.println("hello");
+        System.out.println("hello");
+
     }
 
 
