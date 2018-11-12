6b4152593d1ec9b096c02c2dce2b9f0f23e41afc
Pauuukin <oleg.paukin@mail.ru>
2018-11-12 21:48:19 +0300

    add button action and call "edit" layout

--- src/sample/controllers/MainController.java
+++ src/sample/controllers/MainController.java
@@ -1,5 +1,31 @@
 package sample.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import java.io.IOException;
+
 public class MainController {
 
+    public void showDialog(ActionEvent actionEvent) {
+        try{
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("../fxml/edit.fxml"));
+            stage.setTitle("Добавление записи");
+            stage.setMinWidth(300);
+            stage.setMinHeight(150);
+            stage.setResizable(false);
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(((Node)actionEvent.getSource()).getScene().getWindow()); // определяем родителское окно
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
