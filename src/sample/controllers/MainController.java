ae4428fe31e27a6dacebb98a2234a1f85793c008
Pauuukin <oleg.paukin@mail.ru>
2018-11-16 15:14:32 +0300

    add fx:id for button

--- src/sample/controllers/MainController.java
+++ src/sample/controllers/MainController.java
@@ -1,9 +1,11 @@
 package sample.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,8 +13,16 @@ import java.io.IOException;
 
 public class MainController {
 
+
+    @FXML
+    private Button addButton;     //аннотация @FXML позволяет использовать в контроллере
+                                  //связанную с .fxml файлом переменную, которая работает с конкретным компонентом
+
     public void showDialog(ActionEvent actionEvent) {
         try{
+
+            addButton.setText("clicked!");
+
             Stage stage = new Stage();
             Parent root = FXMLLoader.load(getClass().getResource("../fxml/edit.fxml"));
             stage.setTitle("Добавление записи");
